4924fbe88e615def884668418f26707c2e828c8b
CJC0512 <ehdchs2006@gmail.com>
2024-04-01 15:14:06 +0900

    [feat] OmokLens Vue.js Front 개발환경구축

---
+++ OmokLens-frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <h3>정상 출력 문자입니다.</h3>
+</template>
+
+<style scoped>
+</style>
---
+++ OmokLens-frontend/src/components/sample.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>sample 코드입니다. 편하게 활용하시면 됩니다.</h3>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
